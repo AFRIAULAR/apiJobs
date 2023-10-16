fffe436ad307a30c1f1724cf7994a18eada2264d
AFRIAULAR <aularafrica@gmail.com>
2023-10-16 15:31:40 -0300

    change createJob, add new options

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,9 @@ const saveJob = () => {
       discount: $("#discount").checked,
       medicalInsurance: $("#medicalInsurance").checked,
     },
-    languages: Array.from($$('[type="checkbox"]:checked')).map((checkbox) => checkbox.value),
+    languages: Array.from($$('[type="checkbox"]:checked'))
+      .map((checkbox) => checkbox.value)
+    
   };
   return formData;
 };
@@ -43,6 +45,7 @@ const saveEditedJob = () => {
   };
 };
 
+
 const jobToEdit = (data) => {
   const idJob = $("#btnEditJob").getAttribute("data-id");
   $("#btnEditJob").setAttribute("data-id", idJob);
@@ -54,22 +57,26 @@ const jobToEdit = (data) => {
 
 // DOM
 const createJob = (job) => {
-  const { id, jobName, location, seniority, availability, category } = job;
+  const { id, jobName, location, category, requirements, salary, benefits, languages } = job;
   const cardElement = document.createElement('div');
   cardElement.className = 'w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4';
+  const selectedBenefits = Object.keys(benefits).filter(key => benefits[key]);
+
   cardElement.innerHTML = `
     <div class="c-card block bg-[#f5f5f5] shadow-md hover:shadow-xl rounded-lg overflow-hidden">
       <div class="relative pb-48 overflow-hidden">
         <img class="absolute inset-0 h-full w-full object-cover" src="./assets/images/pia_leon.jpg" alt="">
       </div>
       <div class="p-4">
-        <span class="inline-block px-2 py-1 leading-none bg-[#5BA6A6] text-white rounded-full font-semibold uppercase tracking-wide text-xs">${category}</span>
         <h2 class="mt-2 mb-2 font-bold">${jobName}</h2>
-        <p class="text-sm">${seniority}</p>
+        <p class="text-sm">${salary}</p>
         <div class="mt-3 flex items-center">
           <span class="font-bold text-xl">${location}</span>
         </div>
-        <p class="text-sm">${availability}</p>
+        <p class="text-sm">${requirements}</p>
+        <p class="text-sm">Beneficios: ${selectedBenefits.join(', ')}</p>
+        <p class="text-sm">Idiomas: ${languages.join(', ')}</p>
+        <span class="inline-block px-2 py-2 leading-none bg-[#5BA6A6] text-white rounded-full font-semibold uppercase tracking-wide text-xs">${category}</span>
       </div>
       <div class="p-4 border-t border-b text-xs text-gray-700">
         <button id="btnJobDetails" data-id="${id}" class="btnJobDetails flex items-center bg-[#431545] rounded-full font-semibold px-4 py-2 text-white ml-[70%]">Ver detalles</button>
