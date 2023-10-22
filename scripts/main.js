327eb04e1468c41d736f0e0ce3e9543537817b9a
AFRIAULAR <aularafrica@gmail.com>
2023-10-22 00:11:54 -0300

    change palette color

--- scripts/main.js
+++ scripts/main.js
@@ -120,10 +120,10 @@ const createJob = (job) => {
         <p class="text-sm">${requirements}</p>
         <p class="text-sm">Beneficios: ${selectedBenefits.join(', ')}</p>
         <p class="text-sm">Idiomas: ${languages.join(', ')}</p>
-        <span class="inline-block px-2 py-2 leading-none bg-[#5BA6A6] text-white rounded-full font-semibold uppercase tracking-wide text-xs">${category}</span>
+        <span class="inline-block px-2 py-2 leading-none bg-[#fed766] text-[#011f4b] rounded-full font-semibold uppercase tracking-wide text-xs">${category}</span>
       </div>
       <div class="p-4 border-t border-b text-xs text-gray-700">
-        <button id="btnJobDetails" data-id="${id}" class="btnJobDetails flex items-center bg-[#431545] rounded-full font-semibold px-4 py-2 text-white ml-[70%]">Ver detalles</button>
+        <button id="btnJobDetails" data-id="${id}" class="btnJobDetails flex items-center bg-[#011f4b] rounded-full font-medium px-4 py-2 text-white ml-[70%]">Ver detalles</button>
       </div>
     </div>
   `;
@@ -175,11 +175,11 @@ const btnEditDelete = (id)=>{
   const buttonsContainer = $("#buttonsContainer");
   buttonsContainer.innerHTML = `
     <button type="button" id="btnEditJob" data-id="${id}"
-        class="btnEditJob py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-[#5BA6A6] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-[#431545] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-[#45818e]">
+        class="btnEditJob py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-[#84c1ff] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-[#011f4b] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-[#45818e]">
         Editar
     </button>
     <button type="button" id="btnDeleteJob" data-id="${id}"
-        class="btnDeleteJob block py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-[#f5f5f5] focus:outline-none bg-[#0d191c] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-[#431545] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        class="btnDeleteJob block py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-[#f5f5f5] focus:outline-none bg-[#0d191c] rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-[#011f4b] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
         data-modal-toggle="delete-modal">
         Borrar
     </button>
